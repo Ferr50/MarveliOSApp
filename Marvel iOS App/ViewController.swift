8bd76d47b2e87deac2b48bad0df61c15dc2b2637
Fernando Celestrino <ribeiro.fer50@gmail.com>
2022-06-05 23:56:52 -0300

    constraint new fix

--- Marvel iOS App/ViewController.swift
+++ Marvel iOS App/ViewController.swift
@@ -9,6 +9,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         view.backgroundColor = .blue
         //titleLabel.text = HeroDetailConstants.name
-        titleLabel.text = MarvelAPI().fetchHeroesListing().last?.name ?? ""
+        //titleLabel.text = MarvelAPI().fetchHeroesListing().last?.name ?? ""
     }
 }
