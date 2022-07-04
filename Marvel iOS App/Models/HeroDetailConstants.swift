44d2b84012b561f92ae798755c0e3d4fd7a8a301
Fernando Celestrino <ribeiro.fer50@gmail.com>
2022-07-04 11:13:48 -0300

    app finalizado

--- Marvel iOS App/Models/HeroDetailConstants.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-enum HeroDetailConstants {
-    static let name = "Ken Elist"
-    static let comicsSectionTitle = "Comics"
-    static let comicsTitle = "comicsTitle"
-    static let comicsDescription = "comicsDescription"
-}
--- Marvel iOS App/Models/HeroInformation.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-struct HeroInformation {
-    let name: String
-    let imageName: String
-}
